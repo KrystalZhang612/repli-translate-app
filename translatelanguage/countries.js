358bf0de6810729ffc2ec155572ee0e04eb71642
KrystalZhang612 <krystalzhag612@gmail.com>
2022-11-18 03:58:02 -0800

    first commit

---
+++ translatelanguage/countries.js
@@ -0,0 +1,9 @@
+const countries = {
+    "id-ID": "Indonesian",
+    "ar-SA": "Arabic",
+    "de-DE": "German",
+    "it-IT": "Italian",
+    "es-ES": "Spanish",
+    "ja-JP": "Japanese",
+    "en-GB": "English",
+}
---
+++ translatelanguage/script.js
@@ -0,0 +1,48 @@
+const fromText = document.querySelector('.from-text');
+toText = document.querySelector('.to-text');
+exchageIcon = document.querySelector(".change");
+selectTag = document.querySelectorAll("select");
+icons = document.querySelectorAll(".row i");
+translateBtn = document.querySelector("button");
+
+selectTag.forEach((tag, id) => {
+    for (let country_code in countries) {
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "ja-JP" ? "selected" : "";
+        let option = `<option ${selected} value = "${country_code}"> ${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+
+exchageIcon.addEventListener("click", () => {
+    let tempText = fromText.value;
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    toText.value = tempText;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang;
+});
+
+fromText.addEventListener("keyup", () => {
+    if (!fromText.value) {
+        toTextvalue = "";
+    }
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value.trim();
+    translateFrom = selectTag[0].value;
+    translateTo = selectTag[1].value;
+    if (!text) return;
+    toText.setAttribute("placeholder", "Translating...");
+    let apiUrl = `https://api.mymemory.translated.net/get?
+    q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        toText.value = data.responseData.translatedText;
+        data.matches.forEach(data => {
+            if (data.id === 0) {
+                toText.value = data.translation;
+            }
+        });
+        toText.setAttribute("placeholder", "Translation");
+    });
+}); 
