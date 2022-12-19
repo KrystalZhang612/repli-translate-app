206050c83b472668bb98ae840f9fd94a82c3081e
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-12-18 19:52:53 -0800

    Delete translatelanguage directory

--- translatelanguage/countries.js
+++
@@ -1,9 +0,0 @@
-const countries = {
-    "id-ID": "Indonesian",
-    "ar-SA": "Arabic",
-    "de-DE": "German",
-    "it-IT": "Italian",
-    "es-ES": "Spanish",
-    "ja-JP": "Japanese",
-    "en-GB": "English",
-}
